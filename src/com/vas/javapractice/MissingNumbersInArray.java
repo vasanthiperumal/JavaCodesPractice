d4c30a1f827823cf203dbb9c828ac180b9467d13
vasanthiperumal <speaktovasanthi@gmail.com>
2023-06-02 18:19:05 -0400

    add files on 02jun2023

--- src/com/vas/javapractice/MissingNumbersInArray.java
+++ src/com/vas/javapractice/MissingNumbersInArray.java
@@ -1,38 +1,40 @@
 package com.vas.javapractice;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 public class MissingNumbersInArray {
-
-	public static void main(String[] args) {
-		
-			int[] arr={3,7,8,5,4,15,12};
-			
-			//List<int[]> arrList= new ArrayList<int[]>(Arrays.asList(arr));
-			
-		//	List arrList1=new ArrayList(Arrays.asList(arr));
-		//	System.out.println(arrList1);
-			List<Integer> arrayList1=new ArrayList<>();
-			arrayList1.
-			
-			
-		    Collection<Integer> c = Arrays.asList(arr);
-			
-			List<Integer> arrayList2=new ArrayList<>();
-			
-			for(int i=1;i<=15;i++)
-			{
-				System.out.println(arrayList1.contains(i));
-			if(!arrayList1.contains("i"))
-			{
-			arrayList2.add(i);
-			}
-			}
-			System.out.println(arrayList2);
-		
-	}
-
+ 
+  // Function to find the missing elements
+  public static void printMissingElements(int arr[], int N)
+  {
+    int cnt = 0;
+    for (int i = arr[0]; i <= arr[N - 1]; i++)
+    {
+ 
+      // Check if number is equal to the first element in
+      // given array if array element match skip it increment for next element
+      if (arr[cnt] == i)
+      {
+ 
+        // Increment the count to check next element
+        cnt++;
+      }
+      else
+      {
+         // Print missing number
+        System.out.print(i + " ");
+      }
+    }
+  }
+ 
+  // Driver Code
+  public static void main (String[] args)
+  {
+ 
+    // Given array arr[]
+    int arr[] = { 6, 7, 10, 11, 13 };
+    int N = arr.length;
+ 
+    // Function Call
+    printMissingElements(arr, N);
+  }
 }
+ 
